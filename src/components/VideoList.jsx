4f3ccd0b624bb630803611752ab18a704253d597
Hack Reactor Student <student@hackreactor.com>
2020-02-18 20:53:35 -0800

    First day's work

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,25 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
+import exampleVideoData from '././data/exampleVideoData.js';
+
+class VideoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+  render() {
+    return (
+      <li>{this.props.video}</li>
+    );
+  }
+}
+var MyVideoList = (props) => (
+  <ul>
+    {props.exampleVideoData.map((video) =>
+      <VideoList VideoListEntry={video} />
+    )}
+  </ul>
 );
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +30,14 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+// var VideoList = () => (
+//   <div className="video-list">
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//   </div>
+// );
+
+
